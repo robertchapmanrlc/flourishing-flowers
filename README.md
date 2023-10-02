053a25ccffb20ac8d5d58d3e439823ec7e1c6176
Robert Chapman <chapmanrobert12@gmail.com>
2023-10-02 18:08:04 -0500

    Initialized project with preliminary dev dependencies

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flourishing Flowers
+This is the frontend for a flower shop website. It's Tech stack includes React, Typescript, and Tailwindcss. 
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="font-lexend">Flourishing Flowers</h1>
+    </>
+  )
+}
+
+export default App
