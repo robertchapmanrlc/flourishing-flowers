71bcb2218ebeebea3275c4bc4ace4becdb102e02
Robert Chapman <chapmanrobert12@gmail.com>
2023-10-02 19:36:54 -0500

    Finished the styling for the navbar on desktop and mobile screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,56 @@
+import { useState } from "react";
+import { Search, ShoppingCart, Menu, X } from "lucide-react";
 
-import { Search, ShoppingCart, Menu } from "lucide-react";
+import { Dialog } from "@headlessui/react";
 
 const categories = ["Just Because", "Birthday", "Anniversary", "Wedding"];
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="lg:px-32 md:px-16 px-4 pt-4 flex flex-row justify-between">
-      <h2 className="font-lexend text-2xl">Flourishing Flowers</h2>
-      <ul className="hidden lg:flex flex-row gap-x-5">
-        {categories.map((category) => (
-          <li className="font-lexend">{category}</li>
-        ))}
-      </ul>
-      <div className="flex flex-row md:gap-x-5 gap-x-2">
-        <Search size={30}/>
-        <ShoppingCart size={30} />
-        <Menu size={30} className="lg:hidden" />
-      </div>
-    </nav>
+    <header>
+      <nav className="lg:px-32 md:px-16 px-4 pt-4 flex flex-row justify-between">
+        <h2 className="font-lexend text-2xl">Flourishing Flowers</h2>
+        <ul className="hidden lg:flex flex-row gap-x-5">
+          {categories.map((category) => (
+            <li className="font-lexend">{category}</li>
+          ))}
+        </ul>
+        <div className="flex flex-row md:gap-x-5 gap-x-2">
+          <Search size={30} />
+          <ShoppingCart size={30} />
+          <button type="button" onClick={() => setIsOpen(true)}>
+            <span className="sr-only">Open main menu</span>
+            <Menu size={30} className="lg:hidden" />
+          </button>
+        </div>
+      </nav>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <Dialog.Panel className="flex flex-col items-end fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <button
+            type="button"
+            className="w-[2.875rem] -m-2.5 rounded-md p-2.5 text-gray-700"
+            onClick={() => setIsOpen(false)}
+          >
+            <span className="sr-only">Close menu</span>
+            <X className="h-6 w-6" aria-hidden="true" />
+          </button>
+          <div className="w-full mt-6">
+            <div className="flex flex-col items-start gap-y-5">
+              {categories.map((category) => (
+                <p className="w-full px-3 py-2 rounded-lg hover:bg-gray-100 text-2xl font-lexend">{category}</p>
+              ))}
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
   );
 }
 
