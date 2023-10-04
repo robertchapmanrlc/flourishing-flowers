67da255b356c12814528a47176ec8d46459b155a
Robert Chapman <chapmanrobert12@gmail.com>
2023-10-03 21:01:02 -0500

    Added the main billboard to the home page

---
+++ src/components/main-billboard.tsx
@@ -0,0 +1,16 @@
+
+function MainBillboard() {
+  return (
+    <div className="w-full rounded-xl overflow-hidden">
+      <div className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover bg-[url('src/assets/banner-1.jpg')]">
+        <div className="h-full w-full flex flex-col md:justify-center justify-start items-center text-center">
+          <div className="font-lexend font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs lg:mt-0 mt-10 ">
+            Beautiful Birthday Flowers
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainBillboard;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
-
+import MainBillboard from "../components/main-billboard";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="lg:px-32 md:px-16 px-4 lg:pt-8 md:pt-4 pt-2 flex flex-col justify-start items-center">
+      <MainBillboard />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
