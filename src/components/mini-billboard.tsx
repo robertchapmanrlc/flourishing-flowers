f5ba6a63d2b242adec2859fde85e4f74c30dc521
Robert Chapman <chapmanrobert12@gmail.com>
2023-10-03 21:51:00 -0500

    Hides the two mini billboards on screens smaller than desktop devices

--- src/components/mini-billboard.tsx
+++ src/components/mini-billboard.tsx
@@ -7,13 +7,13 @@ interface MiniBillboardProps {
 
 function MiniBillboard({ item }: MiniBillboardProps) {
   return (
-    <div className="w-full rounded-xl overflow-hidden">
+    <div className="hidden lg:block w-full rounded-xl overflow-hidden">
       <div
         className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
         style={{ backgroundImage: `url(${item.image_url})` }}
       >
         <div className="h-full w-full flex flex-col justify-center items-center text-center">
-          <div className="font-lexend font-bold text-xl sm:text-2xl lg:text-4xl sm:max-w-xl max-w-xs ">
+          <div className="font-lexend font-bold md:text-2xl lg:text-4xl sm:max-w-xl max-w-xs ">
             {item.label}
           </div>
         </div>
