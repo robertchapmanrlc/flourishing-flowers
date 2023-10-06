ab9395e18c6e81b54d5eeed1413584a9edcf8cc5
Robert Chapman <chapmanrobert12@gmail.com>
2023-10-06 18:53:35 -0500

    Added a plus icon to the product cards when hovering oer them

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,3 +1,4 @@
+import { Plus } from "lucide-react";
 import { Card } from "../../types";
 
 interface ProductCardProps {
@@ -7,12 +8,17 @@ interface ProductCardProps {
 function ProductCard({ card }: ProductCardProps) {
   return (
     <div className="group cursor-pointer">
-      <div className="aspect-h-1 aspect-w-1 w-[200px] md:w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none transition ease-in-out group-hover:opacity-75 lg:max-h-100 xl:max-h-140">
+      <div className="relative border aspect-h-1 aspect-w-1 w-[200px] md:w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none transition ease-in-out group-hover:opacity-75 lg:max-h-100 xl:max-h-140">
         <img
           src={`${card.image_url}`}
           alt="Picture of flower"
           className="object-cover object-center lg:h-full lg:w-full"
         />
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+          <div className="flex justify-end">
+            <Plus size={25} className="bg-pink-300 rounded-full p-1 shadow-md hover:scale-110 transition" />
+          </div>
+        </div>
       </div>
       <div className="mt-4 flex justify-between">
         <div>
