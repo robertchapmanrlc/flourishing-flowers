043f517905d8962a093ce19c3429c23419c327d0
Robert Chapman <chapmanrobert12@gmail.com>
2023-10-06 01:47:09 -0500

    Added filters section for the category pages on larger screens

---
+++ src/components/filter.tsx
@@ -0,0 +1,58 @@
+import { Disclosure } from "@headlessui/react";
+import { MinusIcon, PlusIcon } from "lucide-react";
+
+interface FilterProps {
+  id: string;
+  name: string;
+  options: {
+    value: string;
+    label: string;
+    checked: boolean;
+  }[];
+}
+
+function Filter({id, name, options }: FilterProps) {
+  return (
+    <Disclosure as="div" className="border-b border-gray-200 py-6">
+      {({ open }) => (
+        <>
+          <h3 className="-my-3 flow-root">
+            <Disclosure.Button className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+              <span className="font-lexend font-medium text-gray-900">{name}</span>
+              <span className="ml-6 flex items-center">
+                {open ? (
+                  <MinusIcon className="h-5 w-5" aria-hidden="true" />
+                ) : (
+                  <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                )}
+              </span>
+            </Disclosure.Button>
+          </h3>
+          <Disclosure.Panel className="pt-6">
+            <div className="space-y-4">
+              {options.map((option, i) => (
+                <div key={option.value} className="flex items-center">
+                  <input
+                    type="checkbox"
+                    id={`filter-${id}-${i}`}
+                    defaultValue={option.value}
+                    defaultChecked={option.checked}
+                    className="h-4 w-4 rounded border-gray-300 accent-pink-300"
+                  />
+                  <label
+                    htmlFor={`filter-${id}-${i}`}
+                    className="font-lexend ml-3 text-sm text-gray-600"
+                  >
+                    {option.label}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
+
+export default Filter;
--- src/pages/category-page.tsx
+++ src/pages/category-page.tsx
@@ -1,23 +1,49 @@
 import { useState } from "react";
 import { Menu } from "@headlessui/react";
-  
-import { Category } from "../../types"
+
+import { Category } from "../../types";
 import ProductCard from "../components/product-card";
 import { ChevronDownIcon } from "lucide-react";
 import { cn } from "../lib/utils";
+import Filter from "../components/filter";
 
 interface CategoryPageProps {
-    category: Category;
+  category: Category;
 }
 
-function CategoryPage({ category }: CategoryPageProps) {
+const filters = [
+  {
+    id: "color",
+    name: "Color",
+    options: [
+      { value: "white", label: "White", checked: false },
+      { value: "beige", label: "Beige", checked: false },
+      { value: "blue", label: "Blue", checked: false },
+      { value: "brown", label: "Brown", checked: false },
+      { value: "green", label: "Green", checked: false },
+      { value: "purple", label: "Purple", checked: false },
+    ],
+  },
+  {
+    id: "type",
+    name: "Type",
+    options: [
+      { value: "plants", label: "Plants", checked: false },
+      { value: "arrangements", label: "Arrangements", checked: false },
+      { value: "bouquets", label: "Bouquets", checked: false },
+    ],
+  },
+];
 
+function CategoryPage({ category }: CategoryPageProps) {
   const [options, setOptions] = useState([
     { name: "Newest", href: "#", current: true },
     { name: "Price: Low to High", href: "#", current: false },
     { name: "Price: High to Low", href: "#", current: false },
   ]);
-  
+
+  const [colorFilter, setColorFilter] = useState();
+
   const changeActive = (name: string) => {
     let newOptions = [...options];
     newOptions.forEach((option) => {
@@ -28,12 +54,14 @@ function CategoryPage({ category }: CategoryPageProps) {
       }
     });
     setOptions(newOptions);
-  }
+  };
 
   return (
     <div className="lg:px-32 md:px-16 px-4 lg:py-8 md:py-4 py-2 flex flex-col justify-start items-center gap-y-4 md:gap-y-10">
       <div className="w-full flex flex-row justify-between border-b border-gray-200 pb-6 pt-6">
-        <h1 className="text-4xl font-bold font-lexend tracking-tight text-gray-900">{category.label} Flowers</h1>
+        <h1 className="text-4xl font-bold font-lexend tracking-tight text-gray-900">
+          {category.label} Flowers
+        </h1>
         <div className="flex items-center">
           <Menu as="div" className="relative inline-block text-left">
             <div>
@@ -45,16 +73,26 @@ function CategoryPage({ category }: CategoryPageProps) {
                 />
               </Menu.Button>
             </div>
-            <Menu.Items className='absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none'>
+            <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
               <div className="py-1">
                 {options.map((option) => (
-                  <Menu.Item key={option.name} as='div' onClick={() => changeActive(option.name)}>
+                  <Menu.Item
+                    key={option.name}
+                    as="div"
+                    onClick={() => changeActive(option.name)}
+                  >
                     {({ active }) => (
-                      <p className={cn(
-                        option.current ? 'font-medium text-gray-900' : 'text-gray-500',
-                        active && 'bg-gray-100',
-                        'block px-4 py-2 text-sm'
-                      )}>{option.name}</p>
+                      <p
+                        className={cn(
+                          option.current
+                            ? "font-medium text-gray-900"
+                            : "text-gray-500",
+                          active && "bg-gray-100",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        {option.name}
+                      </p>
                     )}
                   </Menu.Item>
                 ))}
@@ -63,13 +101,22 @@ function CategoryPage({ category }: CategoryPageProps) {
           </Menu>
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-x-6 gap-y-10 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {category.cards.map((card, i) => (
-          <ProductCard key={i} card={card} />
-        ))}
+      <div className="pb-24 pt-6">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+          <div className="hidden lg:block">
+            {filters.map((filter, i) => (
+              <Filter key={filter.name} name={filter.name} id={filter.id} options={filters[i].options} />
+            ))}
+          </div>
+          <div className="col-span-3 grid grid-cols-2 gap-x-6 gap-y-10 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {category.cards.map((card, i) => (
+              <ProductCard key={i} card={card} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-export default CategoryPage
+export default CategoryPage;
