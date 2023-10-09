948215a34aad3ca4df20257a43682651fdde0b46
Robert Chapman <chapmanrobert12@gmail.com>
2023-10-08 20:45:36 -0500

    The images of the product cards will lead to the products overview

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -29,11 +29,13 @@ function ProductCard({ card }: ProductCardProps) {
   return (
     <div className="group cursor-pointer">
       <div className="relative aspect-h-1 aspect-w-1 w-[200px] md:w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none transition ease-in-out group-hover:opacity-75 lg:max-h-100 xl:max-h-140">
-        <img
-          src={`${card.image_url}`}
-          alt="Picture of flower"
-          className="object-cover object-center lg:h-full lg:w-full"
-        />
+        <Link to={`/product/${card.product_id}`}>
+          <img
+            src={`${card.image_url}`}
+            alt="Picture of flower"
+            className="object-cover object-center lg:h-full lg:w-full"
+          />
+        </Link>
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex justify-end">
             <Plus size={25} className="bg-pink-300 rounded-full p-1 shadow-md hover:scale-110 transition" onClick={() => addToCart(card.product_id)} />
