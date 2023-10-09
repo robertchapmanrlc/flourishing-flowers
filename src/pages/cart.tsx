3b926e0816658f33f4eddec0df2e757f1d8dc627
Robert Chapman <chapmanrobert12@gmail.com>
2023-10-09 00:48:22 -0500

    Use react-hook-form for preliminary work on handling form submissions

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,22 +1,61 @@
-
 import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { ShoppingCart } from "lucide-react";
 
 import OrderItem from "../components/order-item";
 import { ShopContext } from "../contexts/shop-context";
 import { Order } from "../../types";
-import { ShoppingCart } from "lucide-react";
+
+type FormValues = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  apartment?: string;
+  city: string;
+  country: string;
+  stateOrProvince: string;
+  postalCode: string;
+  phone: string;
+  nameOnCard: string;
+  cardNumber: string;
+  expirationDate: string;
+  cvc: string;
+}
 
 function Cart() {
+
+  const form = useForm<FormValues>({
+    defaultValues: {
+      email: '',
+      firstName: '',
+      lastName: '',
+      address: '',
+      apartment: '',
+      city: '',
+      country: '',
+      stateOrProvince: '',
+      postalCode: '',
+      phone: '',
+      nameOnCard: '',
+      expirationDate: '',
+      cvc: ''
+    }
+  });
+
+  const { register, handleSubmit, formState: { errors } } = form;
+
+  const onSubmit = () => {
+    console.log("Got submission");
+  }
 
   const { cartItems } = useContext(ShopContext);
 
   const items = cartItems.values();
   let orders: Order[] = [];
-  
+
   for (const item of items) {
-    console.log(item);
-    for (let i = 0; i < item?.length; i += 1)
-      orders.push(item[i]);
+    for (let i = 0; i < item?.length; i += 1) orders.push(item[i]);
   }
 
   const subtotal = orders.reduce(
@@ -29,9 +68,11 @@ function Cart() {
   const shipping = 5;
   const total = subtotal + taxes + shipping;
 
-
   return (
-    <div className="w-full lg:px-32 md:px-16 px-4 lg:py-8 md:py-4 py-2 flex flex-col lg:flex-row lg:gap-x-5">
+    <form
+      className="w-full lg:px-32 md:px-16 px-4 lg:py-8 md:py-4 py-2 flex flex-col lg:flex-row lg:gap-x-5"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       {orders.length > 0 ? (
         <>
           <div className="w-full flex flex-col gap-y-10">
@@ -46,9 +87,9 @@ function Cart() {
                 </label>
                 <input
                   type="text"
-                  name="email"
                   id="email"
                   className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                  {...register("email", { required: "Email is required" })}
                 />
               </div>
             </div>
@@ -65,9 +106,11 @@ function Cart() {
                     </label>
                     <input
                       type="text"
-                      name="first name"
                       id="first name"
                       className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                      {...register("firstName", {
+                        required: "First name is required",
+                      })}
                     />
                   </div>
                   <div className="w-full rounded-md shadow-sm">
@@ -79,38 +122,44 @@ function Cart() {
                     </label>
                     <input
                       type="text"
-                      name="last name"
                       id="last name"
                       className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                      {...register("lastName", {
+                        required: "Last name is required",
+                      })}
                     />
                   </div>
                 </div>
                 <div>
                   <label
-                    htmlFor="last name"
+                    htmlFor="address"
                     className="block text-sm font-lexend text-gray-900 pb-2"
                   >
                     Address
                   </label>
                   <input
                     type="text"
-                    name="last name"
-                    id="last name"
+                    id="address"
                     className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                    {...register("address", {
+                      required: "Address is required",
+                    })}
                   />
                 </div>
                 <div>
                   <label
-                    htmlFor="last name"
+                    htmlFor="apartment"
                     className="block text-sm font-lexend text-gray-900 pb-2"
                   >
                     Apartment, suite, etc.
                   </label>
                   <input
                     type="text"
-                    name="last name"
-                    id="last name"
+                    id="apartment"
                     className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                    {...register("apartment", {
+                      required: false,
+                    })}
                   />
                 </div>
                 <div className="flex flex-row gap-x-4">
@@ -123,23 +172,25 @@ function Cart() {
                     </label>
                     <input
                       type="text"
-                      name="city"
                       id="city"
                       className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                      {...register("city", { required: "City is required" })}
                     />
                   </div>
                   <div className="w-full rounded-md shadow-sm">
                     <label
-                      htmlFor="last name"
+                      htmlFor="country"
                       className="block text-sm font-lexend text-gray-900 pb-2"
                     >
                       Country
                     </label>
                     <input
                       type="text"
-                      name="last name"
-                      id="last name"
+                      id="country"
                       className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                      {...register("country", {
+                        required: "Country is required",
+                      })}
                     />
                   </div>
                 </div>
@@ -153,9 +204,11 @@ function Cart() {
                     </label>
                     <input
                       type="text"
-                      name="state/province"
                       id="state/province"
                       className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                      {...register("stateOrProvince", {
+                        required: "State / Province is required",
+                      })}
                     />
                   </div>
                   <div className="w-full rounded-md shadow-sm">
@@ -167,9 +220,11 @@ function Cart() {
                     </label>
                     <input
                       type="text"
-                      name="postal code"
                       id="postal code"
                       className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                      {...register("postalCode", {
+                        required: "Postal Code is required",
+                      })}
                     />
                   </div>
                 </div>
@@ -182,10 +237,11 @@ function Cart() {
                   </label>
                   <input
                     type="tel"
-                    name="phone"
                     id="phone"
                     className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
-                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                    {...register("phone", {
+                      required: "Phone number is required",
+                    })}
                   />
                 </div>
               </div>
@@ -203,9 +259,11 @@ function Cart() {
                     </label>
                     <input
                       type="text"
-                      name="card name"
                       id="card name"
                       className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                      {...register("nameOnCard", {
+                        required: "Card Name is required",
+                      })}
                     />
                   </div>
                   <div className="mt-4 rounded-md shadow-sm">
@@ -217,9 +275,11 @@ function Cart() {
                     </label>
                     <input
                       type="text"
-                      name="card number"
                       id="card number"
                       className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                      {...register("cardNumber", {
+                        required: "Card Number is required",
+                      })}
                     />
                   </div>
                   <div className="flex flex-row gap-x-4">
@@ -232,9 +292,11 @@ function Cart() {
                       </label>
                       <input
                         type="text"
-                        name="expiration date"
                         id="expiration date"
                         className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                        {...register("expirationDate", {
+                          required: "Email is required",
+                        })}
                       />
                     </div>
                     <div className="mt-4 rounded-md shadow-sm">
@@ -246,9 +308,11 @@ function Cart() {
                       </label>
                       <input
                         type="text"
-                        name="cvc"
                         id="cvc"
                         className="w-full rounded-md border-0 py-1.5 pl-1.5 text-gray-900 ring-1 ring-inset ring-pink-300 focus:ring-2 focus:ring-inset focus:ring-pink-500 focus:outline-none"
+                        {...register("cvc", {
+                          required: "CVC is required",
+                        })}
                       />
                     </div>
                   </div>
@@ -290,22 +354,29 @@ function Cart() {
                 <p className="font-lexend">${total}</p>
               </div>
               <div className="flex justify-center p-6">
-                <button className="w-full font-lexend bg-pink-500 hover:bg-pink-600 rounded-md p-3 text-white">
+                <button
+                  type="submit"
+                  className="w-full font-lexend bg-pink-500 hover:bg-pink-600 rounded-md p-3 text-white"
+                >
                   Confirm Order
                 </button>
               </div>
             </div>
           </div>
         </>
-      ) : <>
+      ) : (
+        <>
           <div className="w-full flex justify-center items-center">
             <div className="flex flex-col md:flex-row justify-center items-center md:gap-x-10 lg:gap-x-20 gap-y-5">
               <ShoppingCart size={100} className="text-gray-500" />
-              <h1 className="font-lexend text-xl sm:text-2xl md:text-3xl lg:text-4xl">Your cart is currently empty</h1>
+              <h1 className="font-lexend text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+                Your cart is currently empty
+              </h1>
             </div>
           </div>
-      </>}
-    </div>
+        </>
+      )}
+    </form>
   );
 }
 
