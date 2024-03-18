caba5c50f6d2a700dfc895d3ad2421cc7575c016
Robert Chapman <chapmanrobert12@gmail.com>
2024-03-18 12:34:21 -0500

    Fixed issue module exports issues within config files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 export default function Main() {
-    return <h1>This will be an E-commerece application</h1>
+  return <h1 className="text-3xl">This will be an E-commerece application</h1>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-const colors = require("tailwindcss/colors");
-
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports =  {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
