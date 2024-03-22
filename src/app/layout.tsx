656935a1145b6416483d07febeb305088f54ee0a
Robert Chapman <chapmanrobert12@gmail.com>
2024-03-22 07:32:59 -0500

    Created metadata for the web app.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
-import './index.css';
+import type { Metadata } from "next";
+
+import "./index.css";
+
+export const metadata: Metadata = {
+  title: "Flourishing Flowers",
+  description: "Shop for beautiful flowers for any occasion",
+  authors: [{ name: "Robert", url: "https://robert-chapman.vercel.app" }],
+};
 
 export default function RootLayout({
   children,
