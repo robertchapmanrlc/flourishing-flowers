2905359360e10a8612fcc16eeb8ea8312c9c50fe
Robert Chapman <chapmanrobert12@gmail.com>
2024-03-23 13:04:10 -0500

    Added basic elements for the hero section of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Main() {
-  return <h1 className="text-3xl">This will be an E-commerece application</h1>;
+  return (
+    <main className="w-full flex flex-col items-center">
+      <section
+        className="w-full h-screen bg-cover"
+        style={{ backgroundImage: `url(Banner1.webp)` }}
+      >
+        <h2>Flowers</h2>
+        <h5>Discover the most exquisite blooms for nearly every occasion.</h5>
+        <button>Shop Now</button>
+      </section>
+    </main>
+  );
 }
