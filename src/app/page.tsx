debd51e855e9c1af1ede1f6e76ac18e53058fbb8
Robert Chapman <chapmanrobert12@gmail.com>
2024-03-23 19:56:21 -0500

    Started the basic styling for the hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 export default function Main() {
   return (
     <main className="w-full flex flex-col items-center">
-      <section
-        className="w-full h-screen bg-cover"
-        style={{ backgroundImage: `url(Banner1.webp)` }}
+      <div
+        className="w-full h-96 bg-center bg-cover"
+        style={{ backgroundImage: `url(/Banner1.webp)` }}
       >
-        <h2>Flowers</h2>
-        <h5>Discover the most exquisite blooms for nearly every occasion.</h5>
-        <button>Shop Now</button>
-      </section>
+        <div className="h-full relative flex flex-col items-center justify-center">
+          <h2 className="font-pokova text-6xl">Flowers</h2>
+          <h5 className="text-center font-pokova text-2xl text-black/50">
+            Discover the most exquisite blooms for nearly every occasion.
+          </h5>
+          <button className="">Shop Now</button>
+        </div>
+      </div>
     </main>
   );
 }
