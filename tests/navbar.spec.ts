dcee81baebc16fa7900725e5d796f79c45d8239a
Robert Chapman <chapmanrobert12@gmail.com>
2024-03-23 20:15:37 -0500

    Created test for the hero section of the landing page

--- tests/navbar.spec.ts
+++ tests/navbar.spec.ts
@@ -1,8 +1,26 @@
 import { test, expect } from "@playwright/test";
 
-test("basic test", async ({ page }) => {
-  await page.goto("/");
+test("Contains Title", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await expect(page.getByRole("navigation")).toContainText(
+    "Flourishing Flowers"
+  );
+});
+
+test("Has the navigation links", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await expect(page.getByRole("navigation")).toContainText("Home");
+  await expect(page.getByRole("navigation")).toContainText("Shop");
+  await expect(page.getByRole("navigation")).toContainText("About Us");
+});
+
+test("Icons are visibile", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
   await expect(
-    page.getByRole("heading", { name: "This will be an E-commerece" })
+    page.getByRole("link", { name: "Magnifying glass" })
   ).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Shopping Cart Icon" })
+  ).toBeVisible();
+  await expect(page.getByRole("link", { name: "User Icon" })).toBeVisible();
 });
