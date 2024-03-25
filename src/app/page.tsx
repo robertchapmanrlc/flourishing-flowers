5d7a841114b0f6f7ca379f9e4f06a733b1d0a1c0
Robert Chapman <chapmanrobert12@gmail.com>
2024-03-25 15:33:22 -0500

    Added basic elements for the best sellers section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Main() {
   return (
     <main className="w-full flex flex-col items-center">
-      <div
+      <section
         className="w-full h-96 bg-center bg-cover"
         style={{ backgroundImage: `url(/Banner1.webp)` }}
       >
@@ -14,7 +16,49 @@ export default function Main() {
             Shop Now
           </button>
         </div>
-      </div>
+      </section>
+      <section>
+        <h3>Best Sellers</h3>
+        <div>
+          <Image
+            src="/products/pink-daisy-plant.png"
+            alt="Pink Daisy Plant"
+            width={250}
+            height={250}
+          />
+          <h5>Birthdays</h5>
+        </div>
+        <div>
+          <Image
+            src="/products/red-rose-bouquet.webp"
+            alt="Red Rose Bouquet"
+            width={250}
+            height={250}
+          />
+          <h5>Weddings</h5>
+        </div>
+        <div>
+          <Image
+            src="/products/yellow-rose-plant.png"
+            alt="Yellow Rose Plant"
+            width={250}
+            height={250}
+          />
+          <h5>Get Well</h5>
+        </div>
+        <div>
+          <Image
+            src="/products/pink-rose-plant.png"
+            alt="Pink Rose Plant"
+            width={250}
+            height={250}
+          />
+          <h5>Housewarming</h5>
+        </div>
+        <button className="mt-5 bg-primary py-2 px-4 rounded-lg text-white font-pokova text-3xl">
+          Shop all
+        </button>
+      </section>
     </main>
   );
 }
