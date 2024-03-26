a3c7819aca90a867dd1c6803f969fee13e961616
Robert Chapman <chapmanrobert12@gmail.com>
2024-03-26 13:45:36 -0500

    Created the shop by occasion section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,6 +75,52 @@ export default function Main() {
           </button>
         </div>
       </section>
+      <section className="w-full py-10 flex flex-col items-center justify-center gap-y-10">
+        <div className="w-full px-16 flex justify-between items-center">
+          <h3 className="font-pokova text-4xl">Shop by Occasion</h3>
+          <button className="mt-5 bg-primary py-2 px-4 rounded-lg text-white font-pokova text-3xl">
+            Browse all occasions
+          </button>
+        </div>
+        <div className="w-full flex justify-center gap-x-16">
+          <div className="flex flex-col items-center">
+            <Image
+              src="/products/pink-daisy-plant.png"
+              alt="Pink Daisy Plant"
+              width={250}
+              height={250}
+            />
+            <h5 className="font-pokova text-2xl">Birthdays</h5>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              src="/products/red-rose-bouquet.webp"
+              alt="Red Rose Bouquet"
+              width={250}
+              height={250}
+            />
+            <h5 className="font-pokova text-2xl">Weddings</h5>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              src="/products/yellow-rose-plant.png"
+              alt="Yellow Rose Plant"
+              width={250}
+              height={250}
+            />
+            <h5 className="font-pokova text-2xl">Get Well</h5>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image
+              src="/products/pink-rose-plant.png"
+              alt="Pink Rose Plant"
+              width={250}
+              height={250}
+            />
+            <h5 className="font-pokova text-2xl">Housewarming</h5>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
