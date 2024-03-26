c6c98aba12a13b5947ea5281e8bf050aea218fa2
Robert Chapman <chapmanrobert12@gmail.com>
2024-03-26 13:40:32 -0500

    Fixed the test for the footer

--- tests/footer.spec.ts
+++ tests/footer.spec.ts
@@ -2,10 +2,12 @@ import { test, expect } from "@playwright/test";
 
 test("Footer", async ({ page }) => {
   await page.goto("http://localhost:3000/");
-  await expect(page.locator("h3")).toContainText("Best Sellers");
-  await expect(page.locator("h5")).toContainText("Birthdays");
-  await expect(page.locator("h5")).toContainText("Weddings");
-  await expect(page.locator("h5")).toContainText("Get Well");
-  await expect(page.locator("h5")).toContainText("Housewarming");
-  await expect(page.getByRole("button", { name: "Shop all" })).toBeVisible();
+  await expect(page.locator("h3")).toContainText("Flourishing Flowers");
+  await expect(page.locator("h5")).toContainText("Home");
+  await expect(page.locator("h5")).toContainText("Shop");
+  await expect(page.locator("h5")).toContainText("About Us");
+  await expect(page.getByRole("button", { name: "Contact Us" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "X Icon" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "YouTube Icon" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "Instagram Icon" })).toBeVisible();
 });
