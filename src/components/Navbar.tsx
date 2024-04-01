acc0013d916696575cc70baa1b8c6a28b46569ff
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-01 06:53:17 -0500

    Changed semantic tags of the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,70 +3,77 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex justify-between items-center p-4">
-      <h1 className="text-2xl md:text-4xl font-playball">
-        Flourishing Flowers
-      </h1>
-      <ul className="hidden w-64 md:flex justify-between">
-        <li>
-          <Link
-            href="/"
-            className="text-2xl font-pokova hover:text-primary transition-colors"
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/shop"
-            className="text-2xl font-pokova hover:text-primary transition-colors"
-          >
-            Shop
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/about"
-            className="text-2xl font-pokova hover:text-primary transition-colors"
-          >
-            About Us
-          </Link>
-        </li>
-      </ul>
-      <ul className="w-40 md:w-28 flex justify-between">
-        <li>
-          <Link href="/search">
-            <Image
-              src={"/magnifier.svg"}
-              width={25}
-              height={25}
-              alt="Magnifying glass"
-            />
-          </Link>
-        </li>
-        <li>
-          <Link href="/shopping-cart">
-            <Image
-              src={"/bag-shopping.svg"}
-              width={25}
-              height={25}
-              alt="Shopping Cart Icon"
-            />
-          </Link>
-        </li>
-        <li>
-          <Link href="/account">
-            <Image src={"/user.svg"} width={25} height={25} alt="User Icon" />
-          </Link>
-        </li>
-        <Image
-          src={"/menu.svg"}
-          width={25}
-          height={25}
-          alt="Menu Icon"
-          className="md:hidden cursor-pointer"
-        />
-      </ul>
-    </nav>
+    <header>
+      <nav className="w-full flex justify-between items-center p-4">
+        <h1 className="text-2xl md:text-4xl font-playball">
+          Flourishing Flowers
+        </h1>
+        <ul className="hidden w-64 md:flex justify-between">
+          <li>
+            <Link
+              href="/"
+              className="text-2xl font-pokova hover:text-primary transition-colors"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/shop"
+              className="text-2xl font-pokova hover:text-primary transition-colors"
+            >
+              Shop
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about"
+              className="text-2xl font-pokova hover:text-primary transition-colors"
+            >
+              About Us
+            </Link>
+          </li>
+        </ul>
+        <ul className="w-40 md:w-28 flex justify-between">
+          <li>
+            <Link href="/search">
+              <Image
+                src={"/magnifier.svg"}
+                width={25}
+                height={25}
+                alt="Magnifying glass"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link href="/shopping-cart">
+              <Image
+                src={"/bag-shopping.svg"}
+                width={25}
+                height={25}
+                alt="Shopping Cart Icon"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link href="/account">
+              <Image src={"/user.svg"} width={25} height={25} alt="User Icon" />
+            </Link>
+          </li>
+          <li>
+            <button>
+              <span className="sr-only">Open main menu</span>
+              <Image
+                src={"/menu.svg"}
+                width={25}
+                height={25}
+                alt="Menu Icon"
+                className="md:hidden cursor-pointer"
+              />
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
