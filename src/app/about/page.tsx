a239f984d6676c428260e4ed90441ef2f00ed38f
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-03 09:44:39 -0500

    Added more content to the about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <h1 className="text-xl font-pokova">
         Get To Know Us At Flourishing Flowers
       </h1>
-      <p className="text-sm font-pokova my-12">
+      <p className="text-sm text-center font-pokova my-12">
         We believe flowers can be used to make anything special. From lavish
         weddings that will be remembered for a life time to days you want to
         give a gift just because. All your flower shopping needs can be met
@@ -17,8 +17,13 @@ export default function About() {
         width={3207}
         height={2138}
         alt="Flowers"
-        style={{ borderRadius : '12px'}}
+        style={{ borderRadius: "12px" }}
       />
+      <p className="font-pokova text-sm text-center my-12">
+        Ever since the humble beginnings of our founder as a gardener, we've
+        been one of the best local flowers shops for decades. We strive to be
+        source of beautiful blooms for any and all in the community.
+      </p>
     </main>
   );
 }
--- tests/about.spec.ts
+++ tests/about.spec.ts
@@ -5,4 +5,5 @@ test("About Us Page", async ({ page }) => {
   await expect(page.getByRole('main')).toContainText("Get To Know Us At Flourishing Flowers");
   await expect(page.getByRole('main')).toContainText("We believe flowers can be used to make anything special. From lavish weddings that will be remembered for a life time to days you want to give a gift just because. All your flower shopping needs can be met here.");
   await expect(page.getByRole("img", { name: "Flowers" })).toBeVisible();
+  await expect(page.getByRole('main')).toContainText("Ever since the humble beginnings of our founder as a gardener, we\'ve been one of the best local flowers shops for decades. We strive to be source of beautiful blooms for any and all in the community.");
 });
