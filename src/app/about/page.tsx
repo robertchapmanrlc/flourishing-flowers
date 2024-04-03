08dbc0c0dbcef6eefc223f8d4b4c45edebaf0939
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-03 09:21:07 -0500

    Created basic elements for the about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 export default function About() {
   return (
     <main>
-      <h1>About Us Page</h1>
+      <h1>Get To Know Us At Flourishing Flowers</h1>
+      <p>
+        We believe flowers can be used to make anything special. From lavish
+        weddings that will be remembered for a life time to days you want to
+        give a gift just because. All your flower shopping needs can be met
+        here.
+      </p>
+      <Image
+        src="https://i.ibb.co/StkBmW1/beautiful-background-white-pink-roses.webp"
+        width={3207}
+        height={2138}
+        alt="Flowers"
+      />
     </main>
   );
 }
