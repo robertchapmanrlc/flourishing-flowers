c3d22361a909edc1b646f8909292354315e4eb0a
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-03 09:29:44 -0500

    Stylized the about us page for mobile screens

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <main>
-      <h1>Get To Know Us At Flourishing Flowers</h1>
-      <p>
+    <main className="w-full px-4 py-8">
+      <h1 className="text-xl font-pokova">
+        Get To Know Us At Flourishing Flowers
+      </h1>
+      <p className="text-sm font-pokova my-12">
         We believe flowers can be used to make anything special. From lavish
         weddings that will be remembered for a life time to days you want to
         give a gift just because. All your flower shopping needs can be met
@@ -15,6 +17,7 @@ export default function About() {
         width={3207}
         height={2138}
         alt="Flowers"
+        style={{ borderRadius : '12px'}}
       />
     </main>
   );
