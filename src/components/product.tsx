6099da039ebd455bc7144a1253373087c753396d
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-06 07:41:31 -0500

    Touched the styling of the elements on the product page a bit

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -39,17 +39,17 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
           height={625}
           alt={product.name}
         />
-        <div className="w-full md:max-w-[40%] flex flex-col gap-y-2 md:gap-y-5">
+        <div className="w-full md:max-w-[40%] flex flex-col gap-y-5 md:gap-y-8">
           <h1 className="font-pokova text-3xl md:text-4xl lg:text-5xl">{product.name}</h1>
           <h3 className="font-pokova text-xl md:text-2xl lg:text-3xl">${product.price}</h3>
           <div className="w-full border-b-2" />
           <h3 className="font-pokova font-bold text-xl md:text-2xl lg:text-3xl">Quantity:</h3>
           <div className="w-32 flex flex-row gap-x-5">
-            <button className="bg-primary rounded-md p-1" onClick={() => updateQuantity(-1)}>
+            <button className="bg-primary rounded-md p-1 hover:scale-125 transition-transform" onClick={() => updateQuantity(-1)}>
               <Image src={'/minus.svg'} width={25} height={25} alt="minus"/>
             </button>
             <h4 className="font-h4okova text-lg md:text-xl lg:text-2xl">{quantity}</h4>
-            <button className="bg-primary rounded-md p-1" onClick={() => updateQuantity(1)}>
+            <button className="bg-primary rounded-md p-1 hover:scale-125 transition-transform" onClick={() => updateQuantity(1)}>
               <Image src={'/plus.svg'} width={25} height={25} alt="plus"/>
             </button>
           </div>
@@ -85,7 +85,7 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
               ))}
             </div>
           </RadioGroup>
-          <button className="bg-primary rounded-md py-2 text-white">Add To Cart</button>
+          <button className="bg-primary rounded-md py-2 font-pokova text-xl text-white hover:scale-105 transition-transform">Add To Cart</button>
         </div>
       </div>
       <p className="font-pokova text-left md:text-lg lg:text-2xl">{product.description}</p>
