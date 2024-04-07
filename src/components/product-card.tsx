534583e4b93d427da5988536781ef580c340d866
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-06 21:34:30 -0500

    Added a button that appears when hovering over the product cards to add to the cart

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -17,8 +17,13 @@ export default function ProductCard({ card }: ProductCardProps) {
   };
 
   return (
-    <Link href={`/product/${card.link}`} className="w-full cursor-pointer flex flex-col gap-y-2">
-      <Image src={card.image_url} width={300} height={375} alt={card.name} className="w-full" />
+    <Link href={`/product/${card.link}`} className="group w-full cursor-pointer flex flex-col gap-y-2">
+      <div className="relative w-full">
+        <Image src={card.image_url} width={300} height={375} alt={card.name} className="group-hover:opacity-65 transition-opacity" />
+        <button className="absolute bg-primary rounded-full bottom-2 right-2 p-1 opacity-0 group-hover:opacity-100 hover:scale-125 transition-transform">
+          <Image src={`/plus.svg`} width={18} height={18} alt="plus"/>
+        </button>
+      </div>
       <div className="w-full flex flew-row justify-between">
         <h3 className="font-pokova md:text-xl">{card.name}</h3>
         <h3 className="font-pokova md:text-xl">${card.price}</h3>
