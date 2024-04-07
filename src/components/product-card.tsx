0786d2840200b5f6e1f9314f4da4fa9f5df3fec3
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-06 21:53:18 -0500

    Centered the images on the mobile grids

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -17,8 +17,8 @@ export default function ProductCard({ card }: ProductCardProps) {
   };
 
   return (
-    <Link href={`/product/${card.link}`} className="group w-full cursor-pointer flex flex-col gap-y-2">
-      <div className="relative w-full">
+    <Link href={`/product/${card.link}`} className="group cursor-pointer flex flex-col gap-y-2">
+      <div className="relative w-full flex justify-center">
         <Image src={card.image_url} width={300} height={375} alt={card.name} className="group-hover:opacity-65 transition-opacity" />
         <button className="absolute bg-primary rounded-full bottom-2 right-2 p-1 opacity-0 group-hover:opacity-100 hover:scale-125 transition-transform">
           <Image src={`/plus.svg`} width={18} height={18} alt="plus"/>
