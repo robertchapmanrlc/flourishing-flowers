e580dc960aea144d611781c3da94c82a49eaaef7
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-09 18:13:51 -0500

    Created a database action to get 4 products for the best sellers section

--- src/actions/get-products.ts
+++ src/actions/get-products.ts
@@ -9,6 +9,16 @@ export async function getAllProducts() {
   return products;
 }
 
+export async function getBestSellers() {
+  const products = await database.product.findMany({
+    include: {
+      colors: true
+    },
+    take: 4
+  });
+  return products;
+}
+
 export async function getProduct(productName: string) {
   const product = await database.product.findFirst({
     where: {
