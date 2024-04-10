8694bdf8c3a5a0ea99be29bf0caa1822303682b8
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-10 11:47:03 -0500

    Fixed UI issue where clicking on the links in the sorting menu wouldn't work sometimes

--- src/components/sort-menu.tsx
+++ src/components/sort-menu.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function SortMenu() {
-
   const options = [
     {
       label: "Newest",
@@ -38,15 +37,15 @@ export default function SortMenu() {
       </div>
       <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
         {options.map((option) => (
-          <Menu.Item
-            key={option.label}
-            as="h4"
-            className="px-4 py-2 text-sm hover:bg-gray-100 hover:font-medium"
-          >
-            <Link href={`?${new URLSearchParams({ sort: option.urlLabel })}`}>
+          <Link href={`?${new URLSearchParams({ sort: option.urlLabel })}`}>
+            <Menu.Item
+              key={option.label}
+              as="h4"
+              className="w-full px-4 py-2 text-sm hover:bg-gray-100 hover:font-medium"
+            >
               {option.label}
-            </Link>
-          </Menu.Item>
+            </Menu.Item>
+          </Link>
         ))}
       </Menu.Items>
     </Menu>
