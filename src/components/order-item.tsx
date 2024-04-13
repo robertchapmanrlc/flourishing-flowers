6d1a30bf39e74e09d79e44d5db09dca8fd824222
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-13 09:45:53 -0500

    Turned the list items in the course order list component into their own components

--- src/components/order-item.tsx
+++ src/components/order-item.tsx
@@ -1,62 +1,59 @@
-// import { Trash } from "lucide-react";
-// import { useContext } from "react";
 
-// import { ShopContext } from "../contexts/shop-context";
-import { Link } from "react-router-dom";
+
+import Image from "next/image";
+
+import { Order } from "@/types/types";
 
 interface OrderItemProps {
-  name: string;
-  image_url: string;
-  price: string;
-  quantity: string;
-  color: string;
-  product_id: number;
-  created_at: string;
+  order: Order;
 }
 
-function OrderItem({ name, image_url, price, quantity, color, product_id }: OrderItemProps) {
-
-  // const { removeFromCart, changeQuantity } = useContext(ShopContext);
-
-  // const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-  //   // changeQuantity(product_id, created_at, Number(e.target.value));
-  // };
-
+export default function OrderItem({ order } : OrderItemProps) {
   return (
-    <div className="w-full p-4 flex flex-row justify-between border-b">
-      <div className="flex gap-x-3">
-        <img
-          src={image_url}
-          alt="Picture of product"
-          className="w-[100px] rounded-md border border-gray-900/10"
-        />
-        <div className="flex flex-col justify-between">
-          <div className="flex flex-col justify-start">
-            <Link to={`/product/${product_id}`}>
-              <p className="font-lexend text-gray-900/80 hover:underline cursor-pointer">{name}</p>
-            </Link>
-            <p className="font-lexend text-gray-900/50">{color}</p>
-          </div>
-          <p className="font-lexend">${price}</p>
+    <li
+      key={order.product_id}
+      className="w-full flex items-start border-b-2 py-8 border-b-gray-600/20"
+    >
+      <Image
+        src={order.imageUrl}
+        width={100}
+        height={125}
+        alt={order.name}
+        className="border rounded-lg"
+      />
+      <div className="w-full min-h-28 px-5 flex flex-col md:flex-row justify-between md:justify-start items-center md:items-start">
+        <div className="w-full md:w-1/2">
+          <h5 className="font-pokova font-semibold text-base">
+            {order.name}
+          </h5>
+          <h5 className="font-pokova text-gray-600/70 text-base">
+            {order.color.charAt(0).toUpperCase() + order.color.slice(1)}
+          </h5>
+          <h5 className="font-pokova font-semibold text-base">
+            ${order.price}
+          </h5>
+        </div>
+        <div className="w-full md:w-10 md:min-h-28 flex items-start justify-start">
+          <select
+            name="quantity"
+            defaultValue={order.quantity}
+            id="quantity"
+            className="w-10 p-1 border border-gray-900/20 rounded-md"
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+          </select>
         </div>
       </div>
-      <div className="flex flex-col justify-between items-end">
-        {/* <button onClick={() => removeFromCart(product_id, created_at)}>
-          <Trash className="text-gray-500 hover:text-gray-800" />
-        </button> */}
-        <select name="quantity" defaultValue={quantity} id="quantity" className="p-1 border border-gray-900/20 rounded-md">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-        </select>
-      </div>
-    </div>
+      <button>
+        <Image src={"/x.svg"} width={30} height={30} alt="x" />
+      </button>
+    </li>
   );
 }
-
-export default OrderItem;
--- src/components/order-list.tsx
+++ src/components/order-list.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import Image from "next/image";
-
 import useCartService from "contexts/shop-context";
+import OrderItem from "./order-item";
 
 export default function OrderList() {
 
@@ -16,52 +15,7 @@ export default function OrderList() {
         </p>
       )}
       {orderItems.map((orderItem) => (
-        <li
-          key={orderItem.product_id}
-          className="w-full flex items-start border-b-2 py-8 border-b-gray-600/20"
-        >
-          <Image
-            src={orderItem.imageUrl}
-            width={100}
-            height={125}
-            alt={orderItem.name}
-            className="border rounded-lg"
-          />
-          <div className="w-full min-h-28 px-5 flex flex-col md:flex-row justify-between md:justify-start items-center md:items-start">
-            <div className="w-full md:w-1/2">
-              <h5 className="font-pokova font-semibold text-base">
-                {orderItem.name}
-              </h5>
-              <h5 className="font-pokova text-gray-600/70 text-base">
-                {orderItem.color.charAt(0).toUpperCase() +
-                  orderItem.color.slice(1)}
-              </h5>
-              <h5 className="font-pokova font-semibold text-base">
-                ${orderItem.price}
-              </h5>
-            </div>
-            <div className="w-full md:w-10 md:min-h-28 flex items-start justify-start">
-              <select
-                name="quantity"
-                defaultValue={orderItem.quantity}
-                id="quantity"
-                className="w-10 p-1 border border-gray-900/20 rounded-md"
-              >
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-              </select>
-            </div>
-          </div>
-          <button>
-            <Image src={"/x.svg"} width={30} height={30} alt="x" />
-          </button>
-        </li>
+        <OrderItem order={orderItem} />
       ))}
     </ul>
   );
