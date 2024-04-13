14f0d6995db2e3d7a15eb90f108c1913d1622a06
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-13 09:28:04 -0500

    The list of orders and the checkout section will be display when there are items in the cart

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -1,3 +1,4 @@
+import CheckoutSection from "@/components/checkout-section";
 import OrderList from "@/components/order-list";
 
 
@@ -6,7 +7,10 @@ export default function ShoppingCart() {
     <main className="w-full py-5 px-5 flex flex-col justify-start items-center gap-y-8">
       <h1 className="w-full text-left font-pokova text-4xl">Shopping Cart</h1>
       <div className="w-full h-[2px] bg-gray-600/20" />
-      <OrderList />
+      <div className="w-full flex flex-col lg:flex-row gap-y-10 lg:gap-x-10">
+        <OrderList />
+        <CheckoutSection />
+      </div>
     </main>
   );
 }
--- src/components/order-list.tsx
+++ src/components/order-list.tsx
@@ -11,48 +11,54 @@ export default function OrderList() {
   return (
     <ul className="w-full flex flex-col">
       {orderItems.length === 0 && (
-        <p className=" font-pokova text-xl text-gray-600/60">
+        <p className="font-pokova text-xl text-center text-gray-600/60">
           No orders in the cart.
         </p>
       )}
       {orderItems.map((orderItem) => (
-        <li key={orderItem.product_id} className="w-full flex items-start border-b-2 py-8 border-b-gray-600/20">
+        <li
+          key={orderItem.product_id}
+          className="w-full flex items-start border-b-2 py-8 border-b-gray-600/20"
+        >
           <Image
             src={orderItem.imageUrl}
             width={100}
             height={125}
             alt={orderItem.name}
           />
-          <div className="w-full min-h-28 px-5 flex flex-col justify-between">
-            <div className="w-full">
+          <div className="w-full min-h-28 px-5 flex flex-col md:flex-row justify-between md:justify-start items-center md:items-start">
+            <div className="w-full md:w-1/2">
               <h5 className="font-pokova font-semibold text-base">
                 {orderItem.name}
               </h5>
               <h5 className="font-pokova text-gray-600/70 text-base">
-                {orderItem.color.charAt(0).toUpperCase() + orderItem.color.slice(1)}
+                {orderItem.color.charAt(0).toUpperCase() +
+                  orderItem.color.slice(1)}
               </h5>
               <h5 className="font-pokova font-semibold text-base">
                 ${orderItem.price}
               </h5>
             </div>
-            <select
-              name="quantity"
-              defaultValue={orderItem.quantity}
-              id="quantity"
-              className="w-10 p-1 border border-gray-900/20 rounded-md"
-            >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-            </select>
+            <div className="w-full md:w-10 md:min-h-28 flex items-start justify-start">
+              <select
+                name="quantity"
+                defaultValue={orderItem.quantity}
+                id="quantity"
+                className="w-10 p-1 border border-gray-900/20 rounded-md"
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+              </select>
+            </div>
           </div>
           <button>
-            <Image src={'/x.svg'} width={30} height={30} alt="x" />
+            <Image src={"/x.svg"} width={30} height={30} alt="x" />
           </button>
         </li>
       ))}
